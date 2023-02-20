e42ad43ec3af37e559ac516a85cd422942f5d5d6
Joonas Fernando Koskinen <joonas.f.koskinen@gmail.com>
2023-02-20 07:20:35 +0200

    modified header and index
    Index.php now has the welcoming picture and opening for <section>. This way welcoming pic doesn't collide with login page.

--- header.php
+++ header.php
@@ -64,7 +64,4 @@
         </div>
       </div>
     </nav>
-    <center>
-      <img class="img-fluid" src="../webprogramming23_team3/pictures/backup_hameenlinna.jpg" alt="hameenlinna" id="header-image"/>
-    </center
-    <section>
+    
