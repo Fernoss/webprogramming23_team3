d4aa8496807368aac5a47eb34f32027fdb26d2ab
viktoriaszabo-sz <szaboviktoria.sz03@gmail.com>
2023-02-28 12:19:49 +0200

    Update news_create.php

--- news_create.php
+++ news_create.php
@@ -43,7 +43,7 @@ if (isset($_POST['submit']))
     if (filter_var($email, FILTER_VALIDATE_EMAIL)) //checks whether the email is valid or not
     {
         include 'db.php'; //altered the database php to be able to test 
-    $newemail = $_POST['email'];
+        $newemail = $_POST['email'];
 
     /* COMPARE EMAIL */
 
@@ -57,28 +57,39 @@ if (isset($_POST['submit']))
             {
                 
                 ///////////////////////////////////////////////////////////////////////////////
-                $sql_l = "SELECT logID FROM shammi_login WHERE '{$result['email']}'"; //logid
+                $sql_l = "SELECT logId FROM shammi_login WHERE '{$newemail}'"; //creating a variable from the fetched logId
                 $query_l = mysqli_query($conn, $sql_l);
                 
                 if ($query_l)
                 {
-                    $shammi_logID =  mysqli_fetch_array($query_l);
-                    echo $shammi_logID; 
-                    echo print_r($shammi_logID); //IT WORKED :DDD IT MEANS I CAN ACCESS IT FINALLY
+                    $shammi_logId =  mysqli_fetch_array($query_l);
+                    echo $shammi_logId; 
+                    echo print_r($shammi_logId); 
 
-                    $sql_nl = "INSERT INTO victoria_newletter (news, subscribe, logID)
-                    VALUES ('valami', '1', '$shammi_logID[0]')";
-                    $query = mysqli_query($conn,$sql_nl);
+                    /*$sql_l = "UPDATE victoria_newletter news = 'somesome', subscribe = '1', logId = '{$shammi_logId}'
+                    WHERE {$shammi_logId}";*/
+
+                    $news = 'to be deleted'; 
+                    $subscribe = 1; 
+
+                    $sql= "INSERT INTO victoria_newletter (news, subscribe, logID) 
+                    VALUES ('$news', '$subscribe', $shammi_logId)";                              //this part doesnt work
+                    //$query_nl = mysqli_query($conn, $sql_nl);                                             // this part doesnt work
                     
-                    if ($query)
+                    
+                    if ($conn -> query($sql) === TRUE)
                     {
                         $fname = $result['fName']; 
                         echo "Thank you for subscribing to our newsletter, " . $fname; //+ login name --> personalized message
                     }
                     else 
                     { 
-                        echo "Record not modified"; //if the query couldn't be done, it fails
+                        echo "Error: " .$conn->error; //if the query couldn't be done, it fails
                     }
+                }
+                else 
+                {
+                    echo "Error: " .$conn->error; 
                 }
             }
             else 
